96baec4e41b22b6924ad3ade2ac91a5b07a9e765
666sambo999 <120959947+666sambo999@users.noreply.github.com>
2023-02-07 00:33:42 +0500

    Add files via upload

--- Random2/main.cpp
+++ Random2/main.cpp
@@ -8,7 +8,8 @@ using std::endl;
 # define tab "\t"
 //#define MIN_MAX
 //#define UNICUM
-
+//#define KLASS_PABOTA
+//#define SORTIROVKA
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -68,14 +69,15 @@ void main()
 #endif // UNICUM
 
 
-	// Классная работа
+	// Классная работа - Уникальные числа ; 
 
+#ifdef KLASS_PABOTA
 	const int n = 10;
 	int arr[n];
 	for (int i = 0; i < n; i++)
 	{
 		bool unique; // bool = либо 0 или 1;
-		do
+		do  // делать это пока число не уникально  
 		{
 			arr[i] = rand() % n; // (80 - 70) + 70;
 			unique = true; // Предположим что сгенерировалось уникальное случайное число, но это нужо проверить;
@@ -85,13 +87,13 @@ void main()
 				// arr[j] - перебираемый элеиент 
 				if (arr[i] == arr[j])
 				{
-					unique = false; 
-					break; 
+					unique = false;
+					break;
 				}
 
 			}
-		} while (!unique); 
-		
+		} while (!unique);
+
 	}
 
 	for (int i = 0; i < n; i++)
@@ -99,6 +101,67 @@ void main()
 		cout << arr[i] << tab;
 	}
 	cout << endl;
+#endif // KLASS_PABOTA
+
+	
+	// Дополнения + сортировка массива 
+	
+#ifdef SORTIROVKA
+	const int n = 10;
+	int arr[n];
+	int minRand, maxRand;
+	do
+	{
+
+		cout << "Введите минимальное случайное число: "; cin >> minRand;
+		cout << "Введите максимальное случайное число: "; cin >> maxRand;
+		if (maxRand < minRand) cout << "Error Incorrect values" << endl;
+	} while (maxRand <= minRand);
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+	// Вывод исходного массива: 
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	// Сортировка массива: 
+
+	for (int i = 0; i < n; i++) // i  выбранный 
+	{
+		for (int j = i + 1; j < n; j++) // j -ПЕРЕБИРАЕМЫЙ
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];  // присваеваем зачение i 
+				arr[j] = arr[i]; // меняем местами 
+				arr[j] = buffer;
+			}
+
+		}
+
+	}
+
+	// вывод отсортированного массива:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+#endif // SORTIROVKA
+
+
+
+
+
+
+
+	
+
+
 
 }
 
